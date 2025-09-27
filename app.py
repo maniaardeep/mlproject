47f33445e3ca501629343d59172eb0eac65ffdbd
Mani Kandan <aartipradeepvish05@gmail.com>
2025-09-27 21:25:54 +0530

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-# from src.mlproject.components.data_ingestion import DataIngestion
-# from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
 # from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 # from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
@@ -12,10 +12,10 @@ if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        pass
-        # #data_ingestion_config=DataIngestionConfig()
-        # data_ingestion=DataIngestion()
-        # train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+        ##pass
+        data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         # #data_transformation_config=DataTransformationConfig()
         # data_transformation=DataTransformation()
